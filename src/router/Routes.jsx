57a497f856a855026e86e7e08bcfed08f15b6ed5
Kazi Fabiha Golam Liya <151756377+KaziFabihaLiya@users.noreply.github.com>
2025-11-02 02:34:29 +0600

    Sticky Behaviour Implemented

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router";
 import HomeLayout from "../layouts/HomeLayout";
 import Home from "../pages/Home";
 import CategoryNews from "../pages/CategoryNews";
+import Login from "../pages/Login";
 
 
 const router = createBrowserRouter(
@@ -23,7 +24,18 @@ const router = createBrowserRouter(
         },
         {
             path:"/auth",
-            element: <h2>Authentication Layout</h2>
+            element: <h2>Authentication Layout</h2>,
+            children: [
+                {
+                    path:"/auth/login",
+                    element: <Login></Login>
+                },
+                {
+                    path:"/auth/register",
+                    element: <Login></Login>
+                },
+                
+            ]
         },
         {
             path:"/news",
